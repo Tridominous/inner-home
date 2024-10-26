241d5eb786a778eda97c3e080c622999704babf6
Tridominous <jamesmou02@gmail.com>
2024-10-26 18:30:43 +0100

    Added the navbar

---
+++ src/constants/index.ts
@@ -0,0 +1,48 @@
+export const navLinksLeft = [
+    // {
+    //     id: 1,
+    //     title: 'Profile',
+    //     href: '/',
+    // },
+    {
+        id: 2,
+        title: 'flowers',
+        href: '/flowers',
+    },
+    {
+        id: 3,
+        title: 'ripples',
+        href: '/ripples',
+    },
+    {
+        id: 4,
+        title: '⟫',
+        href: '/arrows',
+    }
+]
+
+export const navLinksRight = [
+    {
+        id: 1,
+        title: 'energy',
+        href: '../../public/assets/seed.png',
+    }, 
+    {
+        id: 2,
+        title: 'flare',
+        href: '../../public/assets/air.png',
+    },
+
+    {
+        id: 3,
+        title: 'drum',
+        href: '../../public/assets/drum.png',
+    },
+
+    {
+        id: 4,
+        title: 'search',
+        href: '../../public/assets/search.png',
+    },
+
+]
